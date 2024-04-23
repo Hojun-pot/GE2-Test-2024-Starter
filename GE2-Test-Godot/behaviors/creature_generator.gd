2fdb27c50045b8297f1203531a72860adce2ea6a
Hojun-Cho <hojuninireland@gmail.com>
2024-04-23 12:08:33 +0100

    End

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -11,29 +11,41 @@ extends Node3D
 
 var head_instance
 var body_instances = []
+func _ready():
+	if not Engine.is_editor_hint():
+		create_creature()
 
 func _process(delta):
 	var time = Time.get_ticks_msec() / 1000.0
-	var sphere_pos = Vector3(0, sin(time * 4), 0)
+	update_creature(time)
 	draw_gizmos()
+
+func update_creature(time):
+	var angle = StartAngle
 	var angle_increment = 2 * PI * Frequency / Length
+	for i in range(len(body_instances)):
+		var size = BaseSize + sin(angle + time * Frequency) * BaseSize * Mult
+		body_instances[i].scale = Vector3(size, size, size)
+		body_instances[i].position = Vector3(i * size, 0, 0) 
+		angle += angle_increment
+
+func create_creature():
+	head_instance = HeadScene.instantiate()
+	add_child(head_instance)
+	head_instance.position = Vector3(0, 0, 0)  
+	head_instance.set("paused", true)
+
+	var previous_instance = head_instance  
+
 	for i in range(Length):
-		var size = BaseSize + sin(StartAngle) * BaseSize * Mult
-		StartAngle += angle_increment
-	
-func _ready():
-	if not Engine.is_editor_hint():
-		head_instance = HeadScene.instantiate()
-		add_child(head_instance)
-		head_instance.set("paused", true)
-		#segment
-		for i in range(Length):
-			var body_instance = BodyScene.instantiate()
-			var size = BaseSize + sin(StartAngle + i * 2 * PI * Frequency / Length) * BaseSize * Mult
-			body_instance.set("size", Vector3(size, size, size))
-			body_instance.set_position(Vector3(i * size, 0, 0))
-			add_child(body_instance)
-			body_instances.append(body_instance)
+		var body_instance = BodyScene.instantiate()
+		var size = BaseSize + sin(StartAngle + i * 2 * PI * Frequency / Length) * BaseSize * Mult
+		body_instance.scale = Vector3(size, size, size)  
+		body_instance.position = Vector3(previous_instance.position.x + size, 0, 0)
+		head_instance.add_child(body_instance)  
+		body_instances.append(body_instance)  
+		previous_instance = body_instance  
+		
 func _input(event):
 	if event.is_action_pressed("p"):
 		head_instance.set("paused", not head_instance.get("paused"))	
@@ -47,25 +59,4 @@ func draw_gizmos():
 	# DebugDraw3D.draw_line(transform.origin, transform.origin + (force * 20), Color(1, 1, 0))
 	# DebugDraw3D.draw_box(sphere_pos, Vector3(1, 2, 1), Color(0, 1, 0))
 	# DebugDraw3D.draw_line(line_begin, line_end, Color(1, 1, 0))
-
-func create_creature():
-	var head = HeadScene.instantiate()
-	var body = BodyScene.instantiate()
-
-	head.size = BaseSize
-	body.size = BaseSize * Mult
-
-	var boid = Boid.new()
-	boid.add_child(CSGBox3D.new())
-	head.add_child(boid)
-	head.paused = true
-
-	for i in range(Frequency):
-		var segment = CSGBox3D.new()
-		segment.size = remap(i, 0, Frequency, BaseSize, BaseSize * Mult)
-		segment.translation = Vector3(0, 0, i * Length / Frequency)
-		add_child(segment)
-		segment.add_child(head)
-		segment.add_child(body)
-#
 
