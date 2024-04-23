327f3f6d570ed715ec962ea01d8196184921317c
Hojun-Cho <hojuninireland@gmail.com>
2024-04-23 11:33:39 +0100

    feild

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -1,15 +1,20 @@
 @tool
 extends Node3D
 
-@export var Length : float	
+var Length : float
 @export var StartAngle : float
 @export var Frequency : float
 @export var BaseSize : float
-@export var Multiplier : float
+@export var Mult : float
 @export var HeadScene : PackedScene
 @export var BodyScene : PackedScene
+
 var force = 5
+var sphere_pos
+
 func _process(delta):
+	var time = Time.get_ticks_msec() / 1000.0
+	sphere_pos = Vector3(0, sin(time * 4), 0)
 	draw_gizmos()
 
 func _ready():
@@ -17,31 +22,31 @@ func _ready():
 		create_creature()
 
 func draw_gizmos():
-	DebugDraw3D.draw_line(transform.origin,  transform.origin + transform.basis.z * 10.0 , Color(0, 0, 1))
-	DebugDraw3D.draw_line(transform.origin,  transform.origin + transform.basis.x * 10.0 , Color(1, 0, 0))
-	DebugDraw3D.draw_line(transform.origin,  transform.origin + transform.basis.y * 10.0 , Color(0, 1, 0))
-	DebugDraw3D.draw_line(transform.origin, transform.origin + (force * 20), Color(1, 1, 0))
-	
+	DebugDraw3D.draw_line(transform.origin, transform.origin + transform.basis.z * 10.0, Color(0, 0, 1))
+	DebugDraw3D.draw_line(transform.origin, transform.origin + transform.basis.x * 10.0, Color(1, 0, 0))
+	DebugDraw3D.draw_line(transform.origin, transform.origin + transform.basis.y * 10.0, Color(0, 1, 0))
+	# DebugDraw3D.draw_line(transform.origin, transform.origin + (force * 20), Color(1, 1, 0))
+	# DebugDraw3D.draw_box(sphere_pos, Vector3(1, 2, 1), Color(0, 1, 0))
+	# DebugDraw3D.draw_line(line_begin, line_end, Color(1, 1, 0))
 
 func create_creature():
-	var head = HeadScene.instance()
-	var body = BodyScene.instance()
+	var head = HeadScene.instantiate()
+	var body = BodyScene.instantiate()
 
 	head.size = BaseSize
-	body.size = BaseSize * Multiplier
+	body.size = BaseSize * Mult
 
-	head.add_child(Boid.new())
-	
-	head.add_child(Harmonic.new())
-	head.add_child(NoiseWander.new())
-	
+	var boid = Boid.new()
+	boid.add_child(CSGBox3D.new())
+	head.add_child(boid)
 	head.paused = true
 
 	for i in range(Frequency):
 		var segment = CSGBox3D.new()
-		segment.size = remap(i, 0, Frequency, BaseSize, BaseSize * Multiplier)
+		segment.size = remap(i, 0, Frequency, BaseSize, BaseSize * Mult)
 		segment.translation = Vector3(0, 0, i * Length / Frequency)
 		add_child(segment)
 		segment.add_child(head)
 		segment.add_child(body)
+#
 
